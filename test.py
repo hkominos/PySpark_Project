8aac6a22512822b3b71556fe26fa630f5cb690aa
hkominos <hkominos@gmail.com>
2015-05-20 10:21:54 +0000

    fixed

--- test.py
+++ test.py
@@ -11,9 +11,9 @@ conf.setAppName("Spark_read")
 sc = SparkContext(conf = conf)
 
 files=sc.textFile("/BAM/*")
-filtered=files.filter(lambda line: -1000<int(line.split("\t")[8])<1000)
+filtered=files.filter(lambda line: abs(int(line.split("\t")[8]))>15)
 countof=files.count()
-filtered.saveAsTextFile("/output.txt")
+filtered.saveAsTextFile("/output2.txt")
 
 
 print "my count is %i" % (countof)
